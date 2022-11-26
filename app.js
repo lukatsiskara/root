28e750f10fa05211f55f6c2f50f80bafc9822df1
lukatsiskara <111465527+lukatsiskara@users.noreply.github.com>
2022-11-26 16:35:51 +0400

    jsonის ფაილით დროებით სათამაშოდ

--- app.js
+++ app.js
@@ -1,140 +1,134 @@
 document.addEventListener('DOMContentLoaded', () => {
   //card options
-  const cardArray = [
-    {
-      frontText: 'ბალახი',
-      backText: 'დაჟარ'
-    },
-    {
-      frontText: 'არის',
-      backText: 'და'
-    },
-    {
-      frontText: 'პირი',
-      backText: 'ბაქ'
-    },
-    {
-      frontText: 'არ',
-      backText: 'ცო'
-    },
-    {
-      frontText: 'აქვს_მაგრამ',
-      backText: 'ბაე'
-    },
-    {
-      frontText: 'კბილები',
-      backText: 'ცარკი'
-    },,
-    {
-      frontText: 'აქვს',
-      backText: 'ჲა'
-    },
-    {
-      backText: 'დაჟარ'
-    },
-    {
-      backText: 'და'
-    },
-    {
-      backText: 'ბაქ'
-    },
-    {
-      backText: 'ცო'
-    },
-    {
-      backText: 'ბაე'
-    },
-    {
-      backText: 'ცარკი'
-    },
-    {
-      backText: 'ჲა'
-    }
-  ]
-
-  cardArray.sort(() => 0.5 - Math.random())  //იმავე მასივში არსებული მონაცემები შემთხვევითად ირევა
-  console.log("hi im console")
-  const grid = document.querySelector('.grid')
-  const dict = document.querySelector('.dict')
-  const pointsDisplay = document.querySelector('#points')
-  let cardsChosen = [] //ინახება შერჩეული 2 ბარათი
-  let cardsChosenId = []  //ინახება შერჩეული 2 ბარათის ID
-  let cardsWon = []  //გამოცნობილი სიტყვები
-  var iSorted = 0  //გამოცნობილი სიტყვების მთვლელი
-  var attempt = 0  //მცდელობების რაოდენობა
-  
-  createBoard()
-
-  //create your board
-  function createBoard() {  //მასივში არეული მონაცემებს იღებს, ქმნის ბარათებს და აწყობს ეკრანზე
-    for (let i = 0; i < cardArray.length; i++) {
-      const card = document.createElement('word')  //ცვლადი თითოეული ახალი ბარათისთვის
-      if(cardArray[i].frontText){  //თუ მასივის წევრს ქართული მნიშვნელობა აქვს, ბარათს საწყის ტექსტს აწერს
-        console.log(cardArray[i].frontText + "two")
-        card.textContent = cardArray[i].frontText, ''
-        console.log(cardArray[i].backText)
-      }else{
-        console.log(cardArray[i].frontText + "one")
-      }
-      card.setAttribute('data-id', i)
-      card.classList.add("word")
-      card.addEventListener('click', flipCard)
-      grid.appendChild(card)
-    }
-  }
-
-  //flip your card
-  function flipCard() {
-    let cardId = this.getAttribute('data-id')
-    console.log(cardId)
-    console.log(cardArray[cardId].backText)
-    cardsChosen.push(cardArray[cardId].backText)
-    cardsChosenId.push(cardId)
-    this.textContent = cardArray[cardId].backText
+  const game = async () => {
     
-    console.log(cardArray[cardId].backText + " " + cardArray[cardId].frontText + " flipcard")
-    if (cardsChosen.length == 2) {
-      setTimeout(checkForMatch, 5)
+    let numbersAmount = 12;
+    const randomNumbers = [];
+    for(let i=0;i<numbersAmount;i++) {
+      randomNumbers.push(Math.floor(Math.random() * 114))
     }
-  }
-  
-  //check for matches
-  function checkForMatch() {
-    const cards = document.querySelectorAll('word')
-    const optionOneId = cardsChosenId[0]
-    const optionTwoId = cardsChosenId[1]    
-    if(cardsChosenId[0] == cardsChosenId[1]) {
-      alert('You have clicked the same image!')
-      console.log("იგივე სურათია")
-      cards[optionOneId].textContent = cardArray[cardsChosenId[0]].frontText
-      cards[optionTwoId].textContent = cardArray[cardsChosenId[1]].frontText      //alert('Sorry, try again')
-    }else if (cardsChosen[0] == cardsChosen[1]) {
-      cards[optionOneId].className = "wonWord"
-      cards[optionTwoId].className = "wonWord"
+    console.log("randomNumbers = "+ randomNumbers)
+    const wordsArrayAll = await fetch('english_words.json').then(resp => resp.json())
+    
+    console.log(wordsArrayAll.length)
+    // const wordsArrayB = []
+    const wordsArray = []
+    
+    for(let i=0;i<numbersAmount;i++) {
+      // wordsArrayB.push(wordsArrayAll[randomNumbers[i]]);
+      wordsArray.push(wordsArrayAll[randomNumbers[i]]);
+    }
 
+    const wordsArrayLenght = wordsArray.length;
+    for(let i=0;i<wordsArrayLenght;i++) {
+      const word = {backText:wordsArray[i].backText}
+      wordsArray.push(word);
+    }
+    console.log(wordsArray)
+
+    wordsArray.sort(() => 0.5 - Math.random())  //იმავე მასივში არსებული მონაცემები შემთხვევითად ირევა
+    
+    const shuffled = document.querySelector('.shuffled')
+    const sorted = document.querySelector('.sorted')
+    const pointsDisplay = document.querySelector('#points')
+    let cardsChosen = [] //ინახება შერჩეული 2 ბარათი
+    let cardsChosenId = []  //ინახება შერჩეული 2 ბარათის ID
+    let cardsWon = []  //გამოცნობილი სიტყვები
+    let iSorted = 0  //გამოცნობილი სიტყვების მთვლელი
+    let attempt = 0  //მცდელობების რაოდენობა
+
+    createBoard()
+
+    //create your board
+    function createBoard() {  //მასივში არეული მონაცემებს იღებს, ქმნის ბარათებს და აწყობს ეკრანზე
+      console.log(wordsArray)
+      for (let i = 0; i < wordsArray.length; i++) {
+        const cardOnBoard = document.createElement('card')  //ცვლადი თითოეული ახალი ბარათისთვის
+        if (wordsArray[i].frontText) {  //თუ მასივის წევრს ქართული მნიშვნელობა აქვს, ბარათს საწყის ტექსტს აწერს
+          cardOnBoard.textContent = wordsArray[i].frontText
+          // console.log(cardOnBoard.textContent)
+        } else {
+          // console.log(wordsArray[i].backText, i)
+        }
+        cardOnBoard.setAttribute('data-id', i)
+        cardOnBoard.classList.add("card")
+        shuffled.appendChild(cardOnBoard)
+        cardOnBoard.addEventListener('click', flipCard)
+      }
+      // const cards = document.getElementsByClassName('card')
+      // console.log("cards - " + cards)
+      // cards.addEventListener('click', flipCard)
+    }
+
+    //flip your card
+    function flipCard() {
+      let cardId = this.getAttribute('data-id')
+      // console.log(cardId)
+
+      cardsChosen.push(wordsArray[cardId])
+      cardsChosenId.push(cardId)
       
-      const translated = document.createElement('translatedWord')  //ცვლადი თითოეული ახალი ბარათისთვის
-      translated.textContent = cardArray[optionTwoId].frontText + " - " + cardArray[optionOneId].backText
-      translated.setAttribute('data-id', iSorted)
-      translated.classList.add("translatedWord")
-      dict.appendChild(translated)
-      cardsWon.push(cardsChosen)
-      iSorted++
-    }
-    else {
-      cards[optionOneId].textContent = cardArray[cardsChosenId[0]].frontText
-      cards[optionTwoId].textContent = cardArray[cardsChosenId[1]].frontText      //alert('Sorry, try again')
-    }
-    cardsChosen = []
-    cardsChosenId = []
-    attempt++
-    pointsDisplay.textContent = `${iSorted}/${attempt} = ${iSorted/attempt}` 
+      this.textContent = wordsArray[cardId].backText
+      
+      // console.log(wordsArray[cardId].backText + " " + wordsArray[cardId].frontText + " flipcard")
+      console.log(cardsChosen)
+      console.log(cardsChosen.length)
+      if (cardsChosen.length == 2) {
+        // console.log(cardsChosen.backText)
+        setTimeout(checkForMatch, 5)
 
-    if  (cardsWon.length == cardArray.length/2) {
-      grid.textContent = "YOU WON"
+      }
+    }
+
+    //check for matches
+    function checkForMatch() {
+      const cards = document.querySelectorAll('card')
+      console.log(cards)
+      const optionOneId = cardsChosenId[0]
+      const optionTwoId = cardsChosenId[1]
+      console.log("შემოწმებაზე", optionOneId, optionTwoId)
+      if (optionOneId === optionTwoId) {
+        alert('You have clicked the same image!')
+        console.log("იგივე ბარათია")
+        cards[optionOneId].textContent = wordsArray[cardsChosenId[0]].frontText
+        cards[optionTwoId].textContent = wordsArray[cardsChosenId[1]].frontText      //alert('Sorry, try again')
+      } else if (cardsChosen[0].backText == cardsChosen[1].backText) {
+        console.log("სიტყვები დაემთხვა")
+        
+        // cards[optionOneId].className = "wonWord"
+        // cards[optionTwoId].className = "wonWord"
+
+
+        const translated = document.createElement('translatedWord')  //ცვლადი თითოეული ახალი ბარათისთვის
+        translated.textContent = wordsArray[optionTwoId].frontText + " - " + wordsArray[optionOneId].backText
+        translated.setAttribute('data-id', iSorted)
+        translated.classList.add("translatedWord")
+        sorted.appendChild(translated)
+        cardsWon.push(cardsChosen)
+        iSorted++
+      }else {
+        cards[optionOneId].textContent = wordsArray[cardsChosenId[0]].frontText
+        cards[optionTwoId].textContent = wordsArray[cardsChosenId[1]].frontText      //alert('Sorry, try again')
+      }
+      cardsChosen = []
+      cardsChosenId = []
+      console.log(cardsChosen, cardsChosenId)
+      attempt++
+      pointsByPercent = `${Math.round(iSorted / attempt * 100)}%`;
+      reversePointsByPercent = `${Math.round(100 - iSorted / attempt * 100)}%`;
+      console.log(pointsByPercent)
+      pointsDisplay.textContent = `${iSorted}/${attempt} = ${Math.round(iSorted / attempt * 100)}%`
+      document.getElementById('dict-score').style.backgroundColor = "hsl(120,0%," + pointsByPercent + ")";
+      document.getElementById('dict-score').style.color = "hsl(120," + reversePointsByPercent + "," + reversePointsByPercent + ")";
+      console.log(cardsWon.length);
+      console.log(wordsArray.length);
+      // console.log(wordsArray);
+
+      if (cardsWon.length == wordsArray.length / 2) {
+        shuffled.textContent = "YOU WON"
+        document.getElementById('gameBoard').style.height = "100px";
+      }
     }
   }
-
-  
-
+  game()
 })
